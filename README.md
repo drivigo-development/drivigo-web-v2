5b1705bd94e9f25e2600e7c0107377ba3ee1004c
Alok-2003 <alok953280@gmail.com>
2025-08-16 09:10:48 +0530

    feat: add PWA support with icons, manifest and service worker configuration

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
+To generate PWA assets run the following command:
+npm run generate-pwa-assets
+
 Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false, // we'll use our custom manifest.webmanifest
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+      }
+    })
+  ],
 })
